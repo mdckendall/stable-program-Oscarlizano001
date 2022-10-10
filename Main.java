2d10f78138fcbe7028e182bfb68309ac819e9bd4
Oscarlizano001 <98754897+Oscarlizano001@users.noreply.github.com>
2022-10-10 07:00:26 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -33,8 +33,7 @@ for(int i =0; i < s.horseArrayList.size();i++){
 System.out.println("Horse #"+(i+1)+" Name: "
 	+ s.horseArrayList.get(i).getName() + " Weight: "
 	+ s.horseArrayList.get(i).getWeight()+ " Tame: "
-	+ s.horseArrayList.get(i).getisTame());
-	
+	+ s.horseArrayList.get(i).getisTame());	
 }
 	  }
   }
